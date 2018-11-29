2639028a964effde81af1384d54a1bf3cad9acac
hellosunnyhello <lianshi.yatou@163.com>
2018-11-29 18:09:01 +0800

    first commit

---
+++ js/help.js
@@ -0,0 +1,36 @@
+$(function () {
+  var currentIndex = 0;
+  initContent()
+  showNavItem();
+  showAnswer();
+  function initContent() {
+    $('#help-wrap .right-content .content-list').eq(currentIndex).css('display','block').siblings().css('display','none');
+  }
+  function showNavItem() {
+    const navItem = $('#help-wrap .left-nav .directory .nav-list .nav-item');
+    navItem.click(function (e) {
+      $(this).addClass('active').siblings().removeClass('active');
+      currentIndex = $(this).index();
+      console.log($(this).index());
+      $('#help-wrap .right-content .content-list').eq(currentIndex).css('display','block').siblings().css('display','none');
+    })
+  }
+  function showAnswer() {
+    var askList = $('#help-wrap .right-content .content-list .content-item .ask')
+    for(let i=0;i<askList.length;i++){
+      askList[i].isShowAnswer = true;
+      askList[i].onclick = function (e) {
+        this.isShowAnswer = !this.isShowAnswer
+        console.log(this.isShowAnswer)
+        //切换样式
+        if(!this.isShowAnswer){
+          $(this).find('.icon').addClass('close')
+          $(this).siblings().css('display','none')
+        }else {
+          $(this).find('.icon').removeClass('close')
+          $(this).siblings().css('display','block')
+        }
+      }
+    }
+  }
+})
